200bdcd8655915df0c7988454841109b12ca743a
afridho <afridho.utama@gmail.com>
2023-07-15 13:54:08 +0700

    Fix linting issue

--- src/search-dropdown.tsx
+++ src/search-dropdown.tsx
@@ -4,46 +4,46 @@ import { useBearDb } from "./hooks";
 
 type Tag = string;
 type TagsDropdownProps = {
-onTagChange: (tag: Tag | null) => void;
+  onTagChange: (tag: Tag | null) => void;
 };
 
 const TagsDropdown = (props: TagsDropdownProps) => {
-const { onTagChange } = props;
-const [db, error] = useBearDb();
-const [tags, setTags] = useState<Tag[]>([]);
+  const { onTagChange } = props;
+  const [db, error] = useBearDb();
+  const [tags, setTags] = useState<Tag[]>([]);
 
-useEffect(() => {
+  useEffect(() => {
     if (db != null) {
-    setTags(db.getTags());
+      setTags(db.getTags());
     }
-}, [db]);
+  }, [db]);
 
-if (error) {
+  if (error) {
     showToast(Toast.Style.Failure, "Something went wrong", error.message);
-}
-return (
+  }
+  return (
     <List.Dropdown
-    tooltip="Select a tag"
-    storeValue={true}
-    onChange={(newValue) => {
+      tooltip="Select a tag"
+      storeValue={true}
+      onChange={(newValue) => {
         if (newValue === "All tags") {
-        onTagChange(null);
-        return;
+          onTagChange(null);
+          return;
         }
         onTagChange(newValue);
-    }}
-    defaultValue="All tags"
+      }}
+      defaultValue="All tags"
     >
-    <List.Dropdown.Section>
+      <List.Dropdown.Section>
         <List.Dropdown.Item key={"All tags"} title={"All tags"} value={"All tags"} />
-    </List.Dropdown.Section>
-    <List.Dropdown.Section title="Tags">
+      </List.Dropdown.Section>
+      <List.Dropdown.Section title="Tags">
         {tags.map((tagTitle) => (
-        <List.Dropdown.Item key={tagTitle} title={tagTitle} value={tagTitle} />
+          <List.Dropdown.Item key={tagTitle} title={tagTitle} value={tagTitle} />
         ))}
-    </List.Dropdown.Section>
+      </List.Dropdown.Section>
     </List.Dropdown>
-);
+  );
 };
 
 export default TagsDropdown;
